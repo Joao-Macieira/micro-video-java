92b9eb17b0cbb2cfceb1335e6e63de3b26c4bc31
João Macieira <jp.macieira@uol.com.br>
2023-03-05 17:29:05 -0300

    feat: implement DDD patterns

---
+++ domain/src/main/java/com/macieira/catalog/admin/domain/Identifier.java
@@ -0,0 +1,5 @@
+package com.macieira.catalog.admin.domain;
+
+public abstract class Identifier extends ValueObject {
+    
+}
---
+++ domain/src/main/java/com/macieira/catalog/admin/domain/ValueObject.java
@@ -0,0 +1,3 @@
+package com.macieira.catalog.admin.domain;
+
+public abstract class ValueObject {}
