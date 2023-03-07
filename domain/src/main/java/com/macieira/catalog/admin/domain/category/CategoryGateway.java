a72477e525f9674547727265ff729330043b666a
João Macieira <jp.macieira@uol.com.br>
2023-03-07 17:20:09 -0300

    feat: category gateway

---
+++ domain/src/main/java/com/macieira/catalog/admin/domain/category/CategoryGateway.java
@@ -0,0 +1,13 @@
+package com.macieira.catalog.admin.domain.category;
+
+import java.util.Optional;
+
+import com.macieira.catalog.admin.domain.pagination.Pagination;
+
+public interface CategoryGateway {
+    Category create(Category aCategory);
+    void deleteById(CategoryID anId);
+    Optional<Category> findById(CategoryID anId);
+    Category update(Category aCategory);
+    Pagination<Category> findAll(CategorySearchQuery aQuery);
+}
